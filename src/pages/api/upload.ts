c54f2e38c1e3490fbc3d60bee23bd43cd8db2b19
Goytia Jeremias <jeremiasjg99@gmail.com>
2024-02-16 21:13:06 -0300

    [upload.ts] error en el metodo uploadStream

--- src/pages/api/upload.ts
+++ src/pages/api/upload.ts
@@ -1,5 +1,33 @@
 import type { APIRoute } from "astro";
 
+import {
+    v2 as cloudinary,
+} from 'cloudinary'
+        
+cloudinary.config({ 
+  cloud_name: 'dj3fgr1rl', 
+  api_key: '866878427149485', 
+  api_secret: 'Vyt_8jBueOCZHaXAwB5Kr2OryLk' 
+});
+
+
+// Ver como solucionar este error
+
+// https://youtu.be/GEfPFLbCXPc?t=3258
+
+const uploadStream = async (buffer: Uint8Array, options: {
+    folder: string
+}) => {
+    return new Promise((resolve, reject) => {
+        cloudinary
+            .uploader
+            .upload_stream(options, (error, result) => {
+                if (result) return resolve (result)
+                reject(error)
+            }).end(buffer)
+    })
+}
+
 export const POST: APIRoute = async ({ request }) => {
     const formData = await request.formData()
     const file = formData.get('file')
